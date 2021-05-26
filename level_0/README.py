ae8579ee6fc81adcf735a01cfa4417095dc5a72b
rrrorrigo <77698169+rrrorrigo@users.noreply.github.com>
2021-05-26 11:11:58 -0300

    Delete README.py

--- level_0/README.py
+++
@@ -1,4 +0,0 @@
-# Project: Hodor - cheat online voting contest
-
-### Write a script or a program that votes 1024 times for your id [here](http://158.69.76.135/level0.php)
-I use the library `selenium`
